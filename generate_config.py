f46490bf1b4767e321265116540efb07cc497762
Scott MAcdonald <smacdonald@salesforce.com>
2019-04-19 09:21:43 -0400

    fixed to meet pep8 standards, and to point to cent7 as default

--- generate_config.py
+++ generate_config.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import argparse
 import os
 import sys
 import yaml
@@ -7,19 +6,19 @@ import yaml
 from jinja2 import Environment, FileSystemLoader
 
 cmdargs = str(sys.argv)
-config_file_name=str(sys.argv[1])
+config_file_name = str(sys.argv[1])
 
-template_folder='templates'
-default_template='centos6-mini.ks.j2'
+template_folder = 'templates'
+default_template = 'centos7-mini.ks.j2'
 
 script_path, script_filename = os.path.split(os.path.abspath(__file__))
-template_folder=os.path.join(script_path,template_folder)
+template_folder = os.path.join(script_path, template_folder)
 
-config_name=os.path.splitext(config_file_name)[0]
+config_name = os.path.splitext(config_file_name)[0]
 
 with open(config_file_name) as config_file_name_fh:
-  dataMap = yaml.load(config_file_name_fh)
-  template_dictionary = dataMap[0]['vars']
+    dataMap = yaml.load(config_file_name_fh)
+    template_dictionary = dataMap[0]['vars']
 
 template_dictionary['work_dir'] = script_path
 template_dictionary['config_name'] = config_name
@@ -30,5 +29,5 @@ output_from_parsed_template = template.render(template_dictionary)
 
 config_name_kickstart = config_name + '.ks'
 
-with open(config_name_kickstart,"wb") as fh:
-  fh.write(output_from_parsed_template)
+with open(config_name_kickstart, "wb") as fh:
+    fh.write(output_from_parsed_template)
